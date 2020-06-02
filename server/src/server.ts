b4a765ca17673a4cfb3639f0c2278f4bf6ea39d7
Marcos Abreu <marcos.abreu.oliveira@gmail.com>
2020-06-02 18:36:14 -0300

    Tipos de requisição no express

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,9 +2,41 @@ import express, { response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
+// Request param: Parâmetros que vem da própria rota que identificamum recurso
+// Query param: Parâmetros quem vem na própria rota geralmente opcionais para filtros, paginação
+// Request Body: Parâmetros para criação/atualização de informação
+
+const users = ["Marcos", "Daniel", "DreSantosz"];
+
 app.get("/users", (req, res) => {
-  console.log("listagem de usuários");
-  res.json(["Marcos", "Daniel", "DreSantoSz"]);
+  // localhost:3333/users?search=on
+  const search = String(req.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  res.json(filteredUsers);
+});
+
+app.post("/users", (req, res) => {
+  const data = req.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return res.json(user);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users[id];
+
+  return res.json({ usuario: user });
 });
 
 app.listen(3333);
